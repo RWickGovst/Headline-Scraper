0e9f6600e4dbfdbcc676cf56570628e0fe181816
Triumph <archie200@icloud.com>
2019-07-24 16:09:03 -0500

    scraping works

--- server.js
+++ server.js
@@ -1,54 +1,75 @@
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// Configure middleware
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-app.get("/all", function(req, res) {
-  db.scrapedData.find({}, function(error, results) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      
-      res.json(results);
-    }
+app.get("/all", (req, res) => {
+  axios.get("https://www.theverge.com/").then ((response) => {
+    var $ = cheerio.load(response.data);
+      $("article h2").each((i, element) => {
+        var result = {}
+        result.title = $(this)
+          .children("a").text();
+        result.link = $(this)
+          .children("a").attr("href");
+
+          db.Article.create(result).then ((dbArticle) => {
+            console.log(dbArticle);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+     res.send("Scrape Complete");
   });
 });
 
 
-app.get("https://www.theverge.com").then(function(response) {
+app.get("/articles", function(req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+    // Find all Notes
+    db.Article.find({}, (err, articles) =>{
+      if(err) {
+        console.log(err);
+      }else {
+        res.json(articles);
+      }
+    })
+});
 
-   var $ = cheerio.load(response.data);
+app.get("/articles/:id", function(req, res) {
 
+  db.Article.findOne({_id: req.params.id}).populate("note").then((dbArticles) => {
+    
+    res.json(dbArticles);
   
-  var results = [];
-
-   $("h2.c-entry-box--compact__title").each(function(i, element) {
-
-    var title = $(element).children().text();
-    var link = $(element).find("a").attr("href");
-
-    results.push({
-      title: title,
-      link: link
-    });
-  });
+})
 });
 
 // Listen on port 3000
