223b9ef02affac28a29c7d5a6eea0922553ee2c7
Triumph <archie200@icloud.com>
2019-07-29 17:58:48 -0500

    make it better

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var mongoose = require("mongoose");
 
 // Require all models
 var db = require("./models");
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperHomework";
 mongoose.connect(MONGODB_URI);
 
-var PORT = 3000;
+var PORT =  process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
